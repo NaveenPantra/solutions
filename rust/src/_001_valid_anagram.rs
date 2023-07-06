67894a24a7f8879ffe29b1b3c8bb26d0cdd9270a
NaveenPantra <naveenpantra.np@gmail.com>
2023-07-06 21:15:19 +0530

    file changes

--- rust/src/_001_valid_anagram.rs
+++ rust/src/_001_valid_anagram.rs
@@ -1,4 +1,3 @@
-use std::hash::Hash;
 use std::str::FromStr;
 use std::collections::HashMap;
 
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -9,8 +9,8 @@ use _006_find_pair_sum::find_pair_sum_caller;
 use _008_grouped_anagrams::group_anagrams_caller;
 
 fn main() {
-    // contains_duplicates_caller();
+    contains_duplicates_caller();
     valid_anagram_caller();
-    // find_pair_sum_caller();
-    // group_anagrams_caller();
+    find_pair_sum_caller();
+    group_anagrams_caller();
 }
