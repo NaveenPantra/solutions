4366a8c5dcbe551984f835672f9f631e7e276fba
NaveenPantra <naveenpantra.np@gmail.com>
2023-08-28 22:28:43 +0530

    js sols

--- javascript/practice/frequencyCounter/_002_anagrams.js
+++ javascript/practice/frequencyCounter/_002_anagrams.js
@@ -6,15 +6,10 @@ function areAnagrams(str1 = '', str2 = '') {
         str2CharsCounterMap[char] = str2CharsCounterMap[char] + 1 || 1
     }
     for (let char of str1) {
-        if (!str2CharsCounterMap[char]) {
-            return false
-        }
+        if (!str2CharsCounterMap[char]) return false
         str2CharsCounterMap[char] -= 1
-        if (str2CharsCounterMap[char] === 0) {
-            delete str2CharsCounterMap[char]
-        }
     }
-    return Object.keys(str2CharsCounterMap).length === 0;
+    return true
 }
 
 export {areAnagrams}
