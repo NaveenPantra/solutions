df6d5265360b0949f182096fa95f948d51ffb9c6
NaveenPantra <naveenpantra.np@gmail.com>
2023-09-05 19:41:41 +0530

    permutation palindrome test improv

--- javascript/tests/practice/permutationPalindrome.test.js
+++ javascript/tests/practice/permutationPalindrome.test.js
@@ -9,4 +9,7 @@ test('permurationPalindrome', () => {
     expect(permutationPalindrome('aaaab')).toBeTruthy()
     expect(permutationPalindrome('ab')).toBeFalsy()
     expect(permutationPalindrome('abcab')).toBeTruthy()
+    expect(permutationPalindrome('taco coa')).toBeFalsy()
+    expect(permutationPalindrome('taco  coa')).toBeTruthy()
+    expect(permutationPalindrome('tacocoa')).toBeTruthy()
 })
